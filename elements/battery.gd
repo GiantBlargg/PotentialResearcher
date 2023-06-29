1bf3909e5d065ca6ba91a9972a68b6c0d169fd34
Daniel Doran <ddoran3@gmail.com>
2023-06-28 20:01:06 -0600

    Powered Objects

--- elements/battery.gd
+++ elements/battery.gd
@@ -1,18 +1,26 @@
 extends RigidBody2D
 
-@export var capacity: float
-var value: float
+@export var charging_time: float = 1
+@export var discharging_time: float = 1
+@export var value: float = 0
 
 var charging: bool
-var discharging: bool
+var discharging: Discharger
+
+var empty:bool
 
 var anim: AnimationPlayer
 
 func set_charging(v:bool):
 	charging = v
 	
-func set_discharging(v:bool):
+func set_discharging(v):
+	if !empty && discharging != null:
+		discharging.stop_power.emit()
 	discharging = v
+	if !empty && discharging != null:
+		discharging.send_power.emit()
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,16 +31,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta:float):
 	if charging:
-		value += delta
-	if discharging:
+		value += delta * (discharging_time / charging_time)
+	if discharging != null:
 		value -= delta
 
-	if value >= capacity:
-		value = capacity
+	if value >= discharging_time:
+		value = discharging_time
 	if value <= 0:
 		value = 0
+		if !empty && discharging != null:
+			discharging.stop_power.emit()
+		empty = true
+	else:
+		empty = false
 		
-	anim.seek(value/capacity, true)
+	anim.seek(value/discharging_time, true)
 	
 
 var carrier:Node2D = null
---
+++ elements/discharger.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+class_name Discharger
+
+signal send_power
+signal stop_power
+
+func _on_body_entered(body):
+	if body.has_method("set_discharging"):
+		body.set_discharging(self)
+
+
+func _on_body_exited(body):
+	if body.has_method("set_discharging"):
+		body.set_discharging(null)
